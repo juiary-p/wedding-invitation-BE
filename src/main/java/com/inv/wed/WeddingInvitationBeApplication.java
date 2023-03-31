ee9e954e687cfb9b7bbe68fa8a1019dac606a985
ellie-du <ellie.du@kakaocorp.com>
2023-03-31 16:26:32 +0900

    test

---
+++ src/main/java/com/inv/wed/WeddingInvitationBeApplication.java
@@ -0,0 +1,13 @@
+package com.inv.wed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingInvitationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingInvitationBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inv/wed/WeddingInvitationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.wed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingInvitationBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
